70485a9af055fb25ba566051fa57034bc0cc004a
hinyb <40139991+hinyb@users.noreply.github.com>
2026-04-08 10:53:47 +0800

    refactor: remove unnecessary check

--- src/main/java/alkalus/main/mixins/early/minecraft/EntityRendererMixin.java
+++ src/main/java/alkalus/main/mixins/early/minecraft/EntityRendererMixin.java
@@ -1,6 +1,5 @@
 package alkalus.main.mixins.early.minecraft;
 
-import alkalus.main.mixins.hooks.EntitySizeManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.EntityLivingBase;
@@ -9,6 +8,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import alkalus.main.mixins.hooks.EntitySizeManager;
 
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
--- src/main/java/alkalus/main/mixins/early/minecraft/NetHandlerPlayClientMixin.java
+++ src/main/java/alkalus/main/mixins/early/minecraft/NetHandlerPlayClientMixin.java
@@ -1,11 +1,8 @@
 package alkalus.main.mixins.early.minecraft;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiSleepMP;
 import net.minecraft.client.network.NetHandlerPlayClient;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
@@ -15,19 +12,12 @@ import alkalus.main.mixins.hooks.EntitySizeManager;
 @Mixin(NetHandlerPlayClient.class)
 public class NetHandlerPlayClientMixin {
 
-    @Shadow
-    private Minecraft gameController;
-
     @ModifyExpressionValue(
             method = "handlePlayerPosLook",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/network/play/server/S08PacketPlayerPosLook;func_148928_d()D"))
     private double applyOffset(double origin) {
-        if (this.gameController.renderViewEntity != null
-                && !(this.gameController.currentScreen instanceof GuiSleepMP)) {
-            return origin - EntitySizeManager.shapeShiftYOffset;
-        }
-        return origin;
+        return origin - EntitySizeManager.shapeShiftYOffset;
     }
 }
